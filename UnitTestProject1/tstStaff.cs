0ed686e5bc4e0eeac5aae10896f7c80c3bf4ea23
Darren Adeboyejo <P2508186@my365.dmu.ac.uk>
2020-02-10 17:27:17 +0000

    Added testing and classes for Staff Management

---
+++ UnitTestProject1/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class I want to create.
+            clsStaff AStaffMember = new clsStaff();
+            //Test to see if it exists.
+            Assert.IsNotNull(AStaffMember);
+        }
+    }
+}
