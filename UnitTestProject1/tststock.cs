cafa80c47d2f6dacd8f79719203d66d225a1314b
Kameron Bains <P2507584@my365.dmu.ac.uk>
2020-02-10 16:57:13 +0000

    Added testing and a class for address management

---
+++ UnitTestProject1/tststock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class tststock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsAddress AnAddress = new clsAddress();
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
